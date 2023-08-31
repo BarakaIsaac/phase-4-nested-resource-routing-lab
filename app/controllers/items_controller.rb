168f0ee823d01b54b7d3431803ab06ebb8457c1b
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-08-31 14:49:31 +0300

    Completed Assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,34 @@ class ItemsController < ApplicationController
     render json: items, include: :user
   end
 
+  def user_items
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items, include: :user, status: :ok
+  end
+
+  def show_user_item
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:id])
+    render json: item, include: :user, status: :ok
+  end
+
+  def create_user_item
+    user = User.find(params[:user_id])
+    item = user.items.build(item_params)
+    
+    if item.save
+      render json: item, include: :user, status: :created
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
-end
+  resources :users, only: [:show] do
+    member do
+      get "/items", to: "items#user_items"
+      get "/items/:id", to: "items#show_user_item"
+    end
+    post "/items", to: "items#create_user_item"
+  end
+  end
